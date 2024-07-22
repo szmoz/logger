3192746aac0a4de7485a04f669e2b9be096e89f8
szmoz <szmoz1992@gmail.com>
2024-07-22 18:01:16 +0200

    Change function outputs

--- logger.go
+++ logger.go
@@ -2,22 +2,22 @@ package logger
 
 import "fmt"
 
-func Info(data interface{}) {
-	fmt.Println("Log info: ", data)
+func Info(message string, params interface{}) {
+	fmt.Println("Log info message: ", message, "params: ", params)
 }
 
-func Error(data interface{}) {
-	fmt.Println("Log error: ", data)
+func Error(message string, params interface{}) {
+	fmt.Println("Log error message: ", message, "params: ", params)
 }
 
-func Warning(data interface{}) {
-	fmt.Println("Log warning: ", data)
+func Warning(message string, params interface{}) {
+	fmt.Println("Log warning message: ", message, "params: ", params)
 }
 
-func Debug(data interface{}) {
-	fmt.Println("Log debug: ", data)
+func Debug(message string, params interface{}) {
+	fmt.Println("Log debug message: ", message, "params: ", params)
 }
 
-func Fatal(data interface{}) {
-	fmt.Println("Lof fatal: ", data)
+func Fatal(message string, params interface{}) {
+	fmt.Println("Lof fatal message: ", message, "params: ", params)
 }
