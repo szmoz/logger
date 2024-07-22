7a572f686ffffbb679d71f1dbba68c1f9508a290
szmoz <szmoz1992@gmail.com>
2024-07-22 17:31:24 +0200

    Add logger

---
+++ logger.go
@@ -0,0 +1,17 @@
+package logger
+import "fmt"
+func Info(data interface{}) {
+	fmt.Println("Log info: ", data)
+}
+
+func Error(data interface{}) {
+	fmt.Println("Log error: ", data)
+}
+
+func Warning(data interface{}) {
+	fmt.Println("Log warning: ", data)
+}
+
+func Debug(data interface{}) {
+	fmt.Println("Log debug: ", data)
+}
